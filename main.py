b407568608823fda5615e0b1610a0b31038dd6e4
im-saurabh <63945720+im-saurabh@users.noreply.github.com>
2021-03-07 21:17:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import re
+
+import pandas as pd
+import tweepy
+from flask import Flask, render_template, request
+from textblob import TextBlob
+
+app = Flask(__name__)
+
+
+@app.route('/sentiment', methods=['GET', 'POST'])
+def sentiment():
+    userid = request.form.get('userid')
+    hashtag = request.form.get('hashtag')
+
+    if userid == "" and hashtag == "":
+        error = "Please Enter any one value"
+        return render_template('index.html', error=error)
+
+    if not userid == "" and not hashtag == "":
+        error = "Both entry not allowed"
+        return render_template('index.html', error=error)
+
+    # ======================Insert Twitter API Here==========================
+    consumerKey = "x7dTt0YMgUb9GS1MF5pSifc4Z"
+    consumerSecret = "K3TCA2vY3rp7NopY2QnMRTRIVRCCR6wS73zNfECsUoo8pXR5Ms"
+    accessToken = "1238852646565376000-R8F2MgJkD2gwwONeiTcwReV0vDRN7z"
+    accessTokenSecret = "1ZzpoMuruKpTGVFRKKB7muOHksAeolCWRxIDFx0AzB1zO"
+    # ======================Insert Twitter API End===========================
+
+    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    authenticate.set_access_token(accessToken, accessTokenSecret)
+    api = tweepy.API(authenticate, wait_on_rate_limit=True)
+
+    def cleanTxt(text):
+        text = re.sub('@[A-Za-z0–9]+', '', text)  # Removing @mentions
+        text = re.sub('#', '', text)  # Removing '#' hash tag
+        text = re.sub('RT[\s]+', '', text)  # Removing RT
+        text = re.sub('https?:\/\/\S+', '', text)  # Removing hyperlink
+        return text
+
+    def getSubjectivity(text):
+        return TextBlob(text).sentiment.subjectivity
+
+    def getPolarity(text):
+        return TextBlob(text).sentiment.polarity
+
+    def getAnalysis(score):
+        if score < 0:
+            return 'Negative'
+        elif score == 0:
+            return 'Neutral'
+        else:
+            return 'Positive'
+
+    if userid == "":
+        # hash tag coding
+        msgs = []
+        msg = []
+        for tweet in tweepy.Cursor(api.search, q=hashtag).items(500):
+            msg = [tweet.text]
+            msg = tuple(msg)
+            msgs.append(msg)
+
+        df = pd.DataFrame(msgs)
+        df['Tweets'] = df[0].apply(cleanTxt)
+        df.drop(0, axis=1, inplace=True)
+        df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
+        df['Polarity'] = df['Tweets'].apply(getPolarity)
+        df['Analysis'] = df['Polarity'].apply(getAnalysis)
+        positive = df.loc[df['Analysis'].str.contains('Positive')]
+        negative = df.loc[df['Analysis'].str.contains('Negative')]
+        neutral = df.loc[df['Analysis'].str.contains('Neutral')]
+
+        positive_per = round((positive.shape[0] / df.shape[0]) * 100, 1)
+        negative_per = round((negative.shape[0] / df.shape[0]) * 100, 1)
+        neutral_per = round((neutral.shape[0] / df.shape[0]) * 100, 1)
+
+        return render_template('sentiment.html', name=hashtag, positive=positive_per, negative=negative_per,
+                               neutral=neutral_per)
+    else:
+        # user coding
+        username = "@" + userid
+
+        post = api.user_timeline(screen_name=userid, count=500, lang="en", tweet_mode="extended")
+        twitter = pd.DataFrame([tweet.full_text for tweet in post], columns=['Tweets'])
+
+        twitter['Tweets'] = twitter['Tweets'].apply(cleanTxt)
+        twitter['Subjectivity'] = twitter['Tweets'].apply(getSubjectivity)
+        twitter['Polarity'] = twitter['Tweets'].apply(getPolarity)
+
+        twitter['Analysis'] = twitter['Polarity'].apply(getAnalysis)
+        positive = twitter.loc[twitter['Analysis'].str.contains('Positive')]
+        negative = twitter.loc[twitter['Analysis'].str.contains('Negative')]
+        neutral = twitter.loc[twitter['Analysis'].str.contains('Neutral')]
+
+        positive_per = round((positive.shape[0] / twitter.shape[0]) * 100, 1)
+        negative_per = round((negative.shape[0] / twitter.shape[0]) * 100, 1)
+        neutral_per = round((neutral.shape[0] / twitter.shape[0]) * 100, 1)
+
+        return render_template('sentiment.html', name=username, positive=positive_per, negative=negative_per,
+                               neutral=neutral_per)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+app.run(debug=True)
